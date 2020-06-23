654787ab6e50802859d2587843fab68a4c605c93
Ahmad Fakhrozy <ahmadfakhrozy@gmail.com>
2020-06-23 22:48:57 +0700

    return xml into  json as response

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,21 @@
 let express = require("express");
 let router = express.Router();
 const axios = require("axios");
+const parser = require("xml2json");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  // res.render('index', { title: 'Express' });
   const url = "https://api.flickr.com/services/feeds/photos_public.gne";
   axios
     .get(url)
     .then((response) => {
-      console.log(response.data);
-
-      res.send("");
-      //res.json(response))
+      //console.log(response.data);
+      var jsonStr = parser.toJson(response.data); //return xml response into json string
+      //console.log("to json -> %s", jsonStr);
+      res.json(JSON.parse(jsonStr)); //return string into json
     })
     .catch((err) => {
-      console.log(err);
-
-      res.status(400).json({ status: "error", message: err });
+      res.status(400).json({ status: err, message: "cannot get data" });
     });
 });
-
 module.exports = router;
