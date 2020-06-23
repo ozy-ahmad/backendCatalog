bf32f6dfc6515c011b06e9d054203b6686f2ceae
Ahmad Fakhrozy <ahmadfakhrozy@gmail.com>
2020-06-23 19:53:38 +0700

    update router to get xml file using axios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
+const axios = require("axios");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+  const url = "https://api.flickr.com/services/feeds/photos_public.gne";
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(response.data);
+
+      res.send("");
+      //res.json(response))
+    })
+    .catch((err) => {
+      console.log(err);
+
+      res.status(400).json({ status: "error", message: err });
+    });
 });
 
 module.exports = router;
