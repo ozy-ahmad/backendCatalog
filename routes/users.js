50c4d5f147248002197c10f6089099bd6125a289
Ahmad Fakhrozy <ahmadfakhrozy@gmail.com>
2020-06-25 04:40:16 +0700

    create routes for user crud

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/User");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get("/get", userController.getAllData);
+router.get("/get/:usersId", userController.getDataById);
+router.delete("/delete/:usersId", userController.deleteById);
 module.exports = router;
